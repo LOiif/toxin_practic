faafb513b52b36c6a2ba4e29a1c82d19ae4c575e
LOiif <loiifmorner@gmail.com>
2021-10-06 14:01:28 +0300

    Импортированы необходимые классы

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import indexHTML from './index.html';
 import formHTML from './form.html';
 import './sass/styles.scss';
 import {MiniFormComponent} from "./scripts/components/mini-form.component";
+import {MainFormComponent} from "./scripts/components/main-form.component";
+import {CatalogComponent} from "./scripts/components/catalog.component";
 
 const miniFormComponent = new MiniFormComponent();
+const mainFormComponent = new MainFormComponent();
+const catalogComponent = new CatalogComponent();
